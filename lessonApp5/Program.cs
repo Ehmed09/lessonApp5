76a4f8b4ce5a3e385f13759862bd251fb9a3001b
askerov.ehmed@mail.ru <askerov.ehmed@mail.ru>
2024-03-28 18:48:51 +0200

    Добавьте файлы проекта.

---
+++ lessonApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//1.В школьной библиотеке дети получают книги.
+//Петя взял 10 книг, Алеша – на 4 книги меньше, чем Петя, а Ваня
+//на 2 книги больше чем Алеша.
+//Сколько книг взял каждый мальчик?
+//Сколько книг они взяли вместе?
+
+int children = 10;
+int mother = 4;
+Console.WriteLine(children - mother);
+int children1 = 6;
+int mother1 = 2;
+Console.WriteLine(children1 + mother1);
+int children2 = 10;
+int mother2 = 8;
+int father = 6;
+Console.WriteLine(children2 + father + mother2);
+
+//2.В первой корзине 23 яблока, а во второй на 11 яблок меньше.
+//Сколько яблок в обеих корзинах?
+
+int b = 23;
+int a = 11;
+int c = 12;
+int e = 23;
+Console.WriteLine(b - a + c + e);
